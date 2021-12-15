6ba0832746b8e9ff5ed0c3413af0099431b2a7b1
Fangyue666 <68301509+Fangyue666@users.noreply.github.com>
2021-12-15 19:29:31 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,21 @@
+public class HelloNumbers {
+    public static void main(String[] args){
+        int x = 0;
+        int i = 0;
+        while (x<46){
+            System.out.print(x+" ");
+            i= i + 1;
+            x = x + i;
+        }
+
+    }
+}
+//public class HelloNumbers {
+//    public static void main(String[] args) {
+//        int x = 0;
+//        while (x < 10) {
+//            System.out.print(x + " ");
+//            x = x + 1;
+//        }
+//    }
+//}
